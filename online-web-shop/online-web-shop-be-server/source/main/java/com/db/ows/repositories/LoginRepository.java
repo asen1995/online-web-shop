7f981fd5252ba94cc196eb529cbec305753cdf98
asenik <asen.nikolaev@nevexis.com>
2018-05-21 23:40:41 +0300

    configure JdbcTemplate

--- online-web-shop/online-web-shop-be-server/source/main/java/com/db/ows/repositories/LoginRepository.java
+++ online-web-shop/online-web-shop-be-server/source/main/java/com/db/ows/repositories/LoginRepository.java
@@ -1,35 +1,27 @@
 package com.db.ows.repositories;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class LoginRepository {
 
 	@Autowired
-	JdbcTemplate jdbcTmpl;
+	NamedParameterJdbcTemplate jdbcTmpl;
 	
-	public void registerUser() {
-		System.out.println("re");
-		System.out.println(jdbcTmpl);
-		System.out.println(jdbcTmpl != null);
+	public void registerUser() {	
 		
-		StringBuilder sql = new StringBuilder();
-		sql.append("INSERT INTO ows_users ");
-		sql.append("(USERNAME, PASSWORD, COUNTRY, USER_STATUS) ");
-		sql.append("values(:username, :password, :country, 1) ");
-
-		Map<String, Object> params = new HashMap<String, Object>();
-		params.put("username", "petur");
-		params.put("password", "otposcih");
-		params.put("country", "rre");	
-
-		jdbcTmpl.update(sql.toString(), params);
-		System.out.println("done");
 	}
 
 }
