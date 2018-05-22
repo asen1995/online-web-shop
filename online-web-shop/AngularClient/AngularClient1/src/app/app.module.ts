2796f9914deb930b4142f36f933a0f86e2346e07
asenik <asen.nikolaev@nevexis.com>
2018-05-22 23:34:35 +0300

    importing form module

--- online-web-shop/AngularClient/AngularClient1/src/app/app.module.ts
+++ online-web-shop/AngularClient/AngularClient1/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms'; 
 import { RegisterComponent } from './components/register/register.component';
 
 
@@ -12,7 +12,8 @@ import { RegisterComponent } from './components/register/register.component';
     RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [RegisterComponent]
