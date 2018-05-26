da6b32f620a503077375075b1fcdc8b7278e1e62
asenik <asen.nikolaev@nevexis.com>
2018-05-26 20:39:40 +0300

    deleted unnused files

--- online-web-shop/AngularClient/AngularClient/src/app/constants.ts
+++
@@ -1,7 +0,0 @@
-export const constants = {
- 
-    REGISTRATION_SUCCESS: 'SUCCESS'
-
-
-  };
-  
--- online-web-shop/AngularClient/AngularClient/src/app/models/User.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    username: string;
-    password: string;
-    country: string;
-    city: string;
-    telephone: string;
-    mail: string;
-    user_state : number;
-  }
