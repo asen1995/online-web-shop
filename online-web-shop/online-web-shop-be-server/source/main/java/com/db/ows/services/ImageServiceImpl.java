49669710e3d27aa55f418c959a793268e61ad019
asenik <asen.nikolaev@nevexis.com>
2018-05-28 14:40:09 +0300

    work in transaction mode

--- online-web-shop/online-web-shop-be-server/source/main/java/com/db/ows/services/ImageServiceImpl.java
+++ online-web-shop/online-web-shop-be-server/source/main/java/com/db/ows/services/ImageServiceImpl.java
@@ -2,11 +2,13 @@ package com.db.ows.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.db.ows.repositories.ImageRepository;
 
 @Service
+@Transactional
 public class ImageServiceImpl implements ImageService {
 
 	@Autowired
--- online-web-shop/online-web-shop-be-server/source/main/java/com/db/ows/services/LoginServiceImpl.java
+++ online-web-shop/online-web-shop-be-server/source/main/java/com/db/ows/services/LoginServiceImpl.java
@@ -2,11 +2,13 @@ package com.db.ows.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.db.ows.model.User;
 import com.db.ows.repositories.LoginRepository;
 
 @Service
+@Transactional
 public class LoginServiceImpl implements LoginService {
 
 	@Autowired
