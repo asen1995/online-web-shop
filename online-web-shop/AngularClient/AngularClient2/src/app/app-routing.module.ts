700bb4dcb5445c63db7c5ee21f01ac0f63f4d121
asenik <asen.nikolaev@nevexis.com>
2018-06-01 17:20:45 +0300

    adding redirection to login page

--- online-web-shop/AngularClient/AngularClient2/src/app/app-routing.module.ts
+++ online-web-shop/AngularClient/AngularClient2/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { HomeComponent } from '../app/components/home/home.component';
 import { AddAdvertisementComponent } from '../app/components/addAdvertisement/addAdvertisement.component';
 import { ProfileComponent } from '../app/components/profile/profile.component'
 const routes: Routes = [
-
+  
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: RegisterLoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'addAdvertisement', component: AddAdvertisementComponent },
